68e46e408389f21335beb67c97b346341a86efde
indahyaniomel <47905963+indahyani@users.noreply.github.com>
2020-02-21 21:48:18 +0700

    Create Grafik.m

---
+++ Grafik.m
@@ -0,0 +1,4 @@
+%pertemuan 3 praktikum
+x=-10:10%nilai x dari -10 sampai 10 dengan selisih 1
+y=x.^2%nilai y adalah x pangkat 2 . penulisan biasanya x^2 ( penggunaan tanda (.) karena nilai x lebi dari satu digit)
+plot(x,y)%plot, merupakan fungsi untuk membuat grafik 2 dimensi
